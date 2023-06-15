81bb148b2f52bc6c45646fdee2fd31a43c68d0c7
Sohail Badghisi <57681550+sohailbadghisi1@users.noreply.github.com>
2023-06-16 00:25:04 +0430

    Update dbconfig.py

--- dbconfig.py
+++ dbconfig.py
@@ -13,7 +13,10 @@ def config(app):
     port = os.getenv("DB_PORT")
     user = os.getenv("DB_USER")
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{user}:{password}@{host}:{port}/{dbName}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://safecoin_user:5l6itOHTqLZe8JURdUnjy2AqNTZWwdmd@dpg-ci5ml65ph6eh6mp9s0rg-a.oregon-postgres.render.com/safecoin"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # TODO It allow to your packages to show their exceptions
+    app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     # TODO It allow to your packages to show their exceptions
     app.config["PROPAGATE_EXCEPTIONS"] = True
