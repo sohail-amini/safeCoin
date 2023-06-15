59a905bf2d9d91684a37586ea7002c14bb90a89f
Sohail Badghisi <57681550+sohailbadghisi1@users.noreply.github.com>
2023-06-16 00:59:26 +0430

    Update dbconfig.py

--- dbconfig.py
+++ dbconfig.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 def config(app):
     password = urllib.parse.quote_plus(os.getenv("DB_PASSWORD"))
@@ -13,10 +16,11 @@ def config(app):
     port = os.getenv("DB_PORT")
     user = os.getenv("DB_USER")
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://safecoin_user:5l6itOHTqLZe8JURdUnjy2AqNTZWwdmd@dpg-ci5ml65ph6eh6mp9s0rg-a.oregon-postgres.render.com/safecoin"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://safecoin_user:5l6itOHTqLZe8JURdUnjy2AqNTZWwdmd@dpg-ci5ml65ph6eh6mp9s0rg-a.oregon-postgres.render.com/safecoin"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     # TODO It allow to your packages to show their exceptions
     app.config["PROPAGATE_EXCEPTIONS"] = True
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # TODO It allow to your packages to show their exceptions
-    app.config["PROPAGATE_EXCEPTIONS"] = True
+    engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+
+    # Define the model
+    Base = declarative_base()
