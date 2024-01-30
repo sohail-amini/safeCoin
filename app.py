f977cb0582311edca6e89fdc09ed93842448bee3
sohailbadghisi1 <sohailbadghisi1@gmail.com>
2024-01-30 13:23:24 +0430

    last commit

--- app.py
+++ app.py
@@ -15,33 +15,33 @@ from sources.settings import settings_bp, add_settings
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
-load_dotenv()
+# load_dotenv()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my_db.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my_db.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.config['JWT_SECRET_KEY'] = '2r4h8s8aF#'
-jwt = JWTManager(app)
-CORS(app)
-config(app)
+# app.config['JWT_SECRET_KEY'] = '2r4h8s8aF#'
+# jwt = JWTManager(app)
+# CORS(app)
+# config(app)
 
-app.register_blueprint(users_bp)
-app.register_blueprint(investment_bp)
-app.register_blueprint(transfer_bp)
-app.register_blueprint(payment_bp)
-app.register_blueprint(product_bp)
-app.register_blueprint(invoice_bp)
-app.register_blueprint(withdraw_bp)
-app.register_blueprint(smtp_bp)
-app.register_blueprint(settings_bp)
+# app.register_blueprint(users_bp)
+# app.register_blueprint(investment_bp)
+# app.register_blueprint(transfer_bp)
+# app.register_blueprint(payment_bp)
+# app.register_blueprint(product_bp)
+# app.register_blueprint(invoice_bp)
+# app.register_blueprint(withdraw_bp)
+# app.register_blueprint(smtp_bp)
+# app.register_blueprint(settings_bp)
 
-db.init_app(app)
+# db.init_app(app)
 
-with app.app_context():
-    db.create_all()
-    create_admin_user()
-    add_settings()
-    add_products_to_database()
+# with app.app_context():
+#     db.create_all()
+#     create_admin_user()
+#     add_settings()
+#     add_products_to_database()
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0",
